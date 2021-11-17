f229f8252ff38eb43fbf66551db77abe3a0eb330
Paul Schlueter <paul@paulschlueter.com>
2021-11-16 18:36:02 -0600

    Add Initial Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { readings } from './readings';
+import { Grid } from '@mui/material';
+import tree from './tree-512.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <img className="treeLogo" src={tree} alt="tree"/>
+          <h1 className="day">Day {readings[0].day}</h1>
+          <div className="date">
+            {new Date().toLocaleDateString('en-US', { dateStyle: 'long' })}
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="readingBody">
+            <h3 className="readingTitle">{readings[0].title}</h3>
+            <p className="subtitle">
+              {readings[0].bibleVerses} | {readings[0].ornamentName}
+            </p>
+            <p className="readingText">{readings[0].readingText}</p>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/readings.ts
+++ src/readings.ts
@@ -1,4 +1,4 @@
-export interface ReadingData {
+export interface ReadingDay {
   day: number;
   title: string;
   ornamentName: string;
@@ -6,12 +6,19 @@ export interface ReadingData {
   readingText: string;
 }
 
-export const readings: ReadingData[] = [
+export const readings: ReadingDay[] = [
   {
     day: 1,
     title: 'Creation of Universe',
     ornamentName: 'Sun',
     bibleVerses: 'Genesis 1-2:4',
     readingText: `If the the world has a beginning, and if it has been created, enquire who gave it this beginning, and who was the Creator: or rather, in the fear that human reasonings may make you wander from the truth, Moses has anticipated enquiry by engraving in our hearts, as a seal and a safeguard, the awful name of God: 'In the beginning God created' - It is He, beneficent Nature, Goodness without measure, a worthy object of love for all beings endowed with reason, the beauty the most to be desired, the origin of all that exists, the source of life, intellectual light, impenetrable wisdom, it is He who 'in the beginning created heaven and earth.'`,
+  },
+  {
+    day: 2,
+    title: 'Creation of Man',
+    ornamentName: 'Two People',
+    bibleVerses: 'Genesis 2:4-25',
+    readingText: `The sun was formed by a mere command, but man by God's hands: 'Let us make man after our image, and after our likeness' (Gen. 1:26). A wooden image of an earthly king is held in honor; how much more a rational image of God? (St. Cyril of Jerusalem, Catechetical Lectures: Lecture 12 no. 5)`,
   },
 ];
