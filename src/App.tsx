c40b9c011a50c8e14741d9c4b7e46412a2a8eb97
Paul Schlueter <paul@paulschlueter.com>
2021-11-20 22:27:04 -0600

    Enable Swiping Between Days

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,29 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import tree from './tree-512.png';
-import { Grid } from '@mui/material';
 import { ReadingDay } from './ReadingDay';
+import SwipeableViews from 'react-swipeable-views';
+import { readings } from './readings';
+
+export const startDate = new Date(2021, 10, 15, 0, 0, 0, 0);
 
 const getDayNumberForToday = (): number => {
-  const startDate = new Date(2021, 10, 15, 0, 0, 0, 0);
   return Math.ceil(
     (new Date().getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24),
   );
 };
 
 function App() {
+  const currentDay = getDayNumberForToday();
+
   return (
     <div className="App">
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <img className="treeLogo" src={tree} alt="tree" />
-        </Grid>
-        <ReadingDay dayNumber={getDayNumberForToday()} />
-      </Grid>
+      <img className="treeLogo" src={tree} alt="tree" />
+      <SwipeableViews enableMouseEvents index={currentDay - 1}>
+        {readings.map((_, i) => (
+          <ReadingDay dayNumber={i + 1} />
+        ))}
+      </SwipeableViews>
     </div>
   );
 }
--- src/ReadingDay.tsx
+++ src/ReadingDay.tsx
@@ -1,16 +1,24 @@
 import styles from './ReadingDay.module.css';
 import { readings } from './readings';
 import { Grid } from '@mui/material';
+import { startDate } from './App';
+
+const addDays = (date: Date, days: number) => {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
 
 export const ReadingDay = ({ dayNumber }: { dayNumber: number }) => {
   const selectedReadings = readings[dayNumber - 1];
+  const readingDate = addDays(startDate, dayNumber - 1);
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <h1 className={styles.day}>Day {selectedReadings.day}</h1>
         <div className={styles.date}>
-          {new Date().toLocaleDateString('en-US', { dateStyle: 'long' })}
+          {readingDate.toLocaleDateString('en-US', { dateStyle: 'long' })}
         </div>
       </Grid>
       <Grid item xs={12}>
